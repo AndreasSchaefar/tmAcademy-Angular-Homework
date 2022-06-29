b1df326b8983ea592fa62b9a60ee0eaba7447f47
AndreasSchaefar <skachko.andrii@gmail.com>
2022-06-29 15:00:10 +0300

    added formsmodule and embedded users into the dom

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './components/users/users.component';
+import { FormsModule } from '@angular/forms';
 import { ActionsComponent } from './components/actions/actions.component';
 import { UserCardComponent } from './components/user-card/user-card.component';
 
 @NgModule({
   declarations: [UsersComponent, ActionsComponent, UserCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [UsersComponent, ActionsComponent],
 })
 export class UsersModule {}
