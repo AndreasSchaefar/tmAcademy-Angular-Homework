29a2ed181895509f8a3b6ed86460ba7fa54dd294
AndreasSchaefar <skachko.andrii@gmail.com>
2022-06-29 10:29:30 +0300

    users module and mock data

---
+++ src/app/users/User.ts
@@ -0,0 +1,7 @@
+export interface IUSer {
+  id?: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+}
---
+++ src/app/users/mock-users.ts
@@ -0,0 +1,39 @@
+import { IUSer } from './User';
+
+export const USERS: IUSer[] = [
+  {
+    id: 1,
+    firstname: '1',
+    lastname: '1',
+    email: '1@gmail.com',
+    phone: '123',
+  },
+  {
+    id: 2,
+    firstname: '2',
+    lastname: '2',
+    email: '2@gmail.com',
+    phone: '223',
+  },
+  {
+    id: 3,
+    firstname: '3',
+    lastname: '3',
+    email: '3@gmail.com',
+    phone: '323',
+  },
+  {
+    id: 4,
+    firstname: '4',
+    lastname: '4',
+    email: '4@gmail.com',
+    phone: '423',
+  },
+  {
+    id: 5,
+    firstname: '5',
+    lastname: '5',
+    email: '5@gmail.com',
+    phone: '523',
+  },
+];
