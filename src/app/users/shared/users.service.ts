6707fb483f22af3fa905778ce2250f595434cd77
AndreasSchaefar <skachko.andrii@gmail.com>
2022-07-01 15:22:52 +0300

    API function for posting new users

--- src/app/users/shared/users.service.ts
+++ src/app/users/shared/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
 
 import { IUSer } from '../User';
 
@@ -11,11 +11,21 @@ export class UsersService {
   private apiUrl: string = 'https://jsonplaceholder.typicode.com/users';
 
   constructor(private http: HttpClient) {}
+
   getUsers(): Observable<IUSer[]> {
     return this.http.get<IUSer[]>(this.apiUrl);
   }
 
   deleteUser(user: IUSer): Observable<IUSer[]> {
     return this.http.delete<IUSer[]>(this.apiUrl + '/' + user.id);
+  }
+
+  createUser(user: IUSer): Observable<IUSer[]> {
+    return this.http.post<IUSer[]>(this.apiUrl + '/' + user.id, user).pipe(
+      catchError(() => {
+        console.log(`The user ${user.name} was posted to ${this.apiUrl}`);
+        return of(<IUSer[]>[]);
+      })
+    );
   }
 }
